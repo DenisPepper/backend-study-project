8c538d5a8114c0d5d2ce70dc83152491a4b509d5
DenisPepper <thech@yandex.ru>
2023-04-06 22:28:06 +0300

    Add global middleware

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import {userRouter} from "./users/users.js";
 const PORT = 9090;
 const app = express();
 
+app.use((request, response, next) => {
+  next();
+});
+
 app.use('/user', userRouter);
 
 app.get('/', (request, response) => {
